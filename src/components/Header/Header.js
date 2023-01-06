8d67fb86dc132c928921d63a87991392c380b5d7
Kams Kanagi <karamo.kanagi@dodoker.com>
2023-01-06 21:40:04 +0800

    updated social icon name

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,9 +5,9 @@ import { DiCssdeck } from 'react-icons/di';
 
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
-const Header = () =>  (
-<Container>
-  {/*
+const Header = () => (
+  <Container>
+    {/*
   <Div1>
     <Link href="/">
       // <a style={{marginBottom:"20px"}}> <span>My Portfolio</span></a>
@@ -15,42 +15,42 @@ const Header = () =>  (
   </Div1>
    */}
 
-  <Div2>
-    <li>
-      <Link href="/">
-      <NavLink> My Portfolio</NavLink>
-      </Link>
-    </li>
-    <li>
-      <Link href="#tech">
-      <NavLink> Technology</NavLink>
-      </Link>
-    </li>
-    <li>
-      <Link href="#about">
-      <NavLink> About</NavLink>
-      </Link>
-    </li>
-    <li>
-      <Link href="#pub">
-      <NavLink> Publications</NavLink>
-      </Link>
-    </li>
-  </Div2>
+    <Div2>
+      <li>
+        <Link href="/">
+          <NavLink> My Portfolio</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#tech">
+          <NavLink> Technology</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#about">
+          <NavLink> About</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#pub">
+          <NavLink> Publications</NavLink>
+        </Link>
+      </li>
+    </Div2>
 
-  <Div3>
-    <SocialIcons href="https://github.com/kamskanagi">
-      <AiFillGithub size="3rem" />
-    </SocialIcons>
-    <SocialIcons href="https://linkedin.com/in/karamo-kanagi-ph-d-14546526/">
-      <AiFillLinkedin size="3rem" />
-    </SocialIcons>
-    <SocialIcons href="https://instagram.com/Pabi">
-      <AiFillInstagram size="3rem" />
-    </SocialIcons>
+    <Div3>
+      <SocialIcons href="https://github.com/kamskanagi">
+        <AiFillGithub size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://linkedin.com/in/karamo-kanagi-ph-d-14546526/">
+        <AiFillLinkedin size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://instagram.com/">
+        <AiFillInstagram size="3rem" />
+      </SocialIcons>
 
-  </Div3>
-</Container>
+    </Div3>
+  </Container>
 );
 
 export default Header;
